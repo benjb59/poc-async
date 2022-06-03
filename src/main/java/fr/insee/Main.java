5bf2859be6835827577c089eaed82d18923310d4
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2022-06-03 15:19:56 +0200

    MOdifs pour utiliser r2dbc

--- src/main/java/fr/insee/Main.java
+++ src/main/java/fr/insee/Main.java
@@ -1,12 +1,53 @@
 package fr.insee;
 
+import fr.insee.pocasync.producer.domain.UserDTO;
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.data.r2dbc.mapping.event.BeforeConvertCallback;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
+
+
+    @Bean
+    public BeforeConvertCallback<UserDTO> idGeneratingCallback(DatabaseClient databaseClient) {
+
+        return (userDTO, sqlIdentifier) -> userDTO.userId() == null ?
+                databaseClient.sql("SELECT NEXT VALUE FOR primary_key")
+                        .map(row -> row.get(0, Long.class))
+                        .first()
+                        .map(userDTO::withUserId):
+                Mono.just(userDTO);
+    }
+
+        @Bean
+    public ConnectionFactoryInitializer databaseSchemaInitializer(ConnectionFactory connectionFactory) {
+
+        var initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(
+                new ResourceDatabasePopulator(
+                        new ByteArrayResource("""
+                create sequence if not exists primary_key;
+                DROP TABLE IF EXISTS user_dto;
+                CREATE TABLE user_dto (user_id bigint PRIMARY KEY, username VARCHAR(100) NOT NULL, correlation_id UUID NOT NULL, registered BOOLEAN);
+                """.getBytes()
+                        )
+                ));
+
+        return initializer;
+    }
+
 }
 
--- src/main/java/fr/insee/pocasync/producer/repository/UserRepository.java
+++ src/main/java/fr/insee/pocasync/producer/repository/UserRepository.java
@@ -2,15 +2,17 @@ package fr.insee.pocasync.producer.repository;
 
 import fr.insee.pocasync.producer.domain.UserDTO;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserDTO, UUID> {
+public interface UserRepository extends ReactiveCrudRepository<UserDTO, UUID> {
 
-    Optional<UserDTO> findByUsername(String username);
-    Optional<UserDTO> findByCorrelationId(UUID correlationId);
+    Mono<UserDTO> findByUsername(String username);
+    Mono<UserDTO> findByCorrelationId(UUID correlationId);
 
 }
